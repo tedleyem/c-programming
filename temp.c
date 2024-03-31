bd35ce2a615b4d25e0b3f527c8364dffdad6a5f0
Tedley Meralus <tmeralus@gmail.com>
2024-03-31 12:13:23 -0400

    fixed syntax for temp check!

--- temp.c
+++ temp.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /* Temperature conversion script */
 /* print Fahrenheit-Celsius table 
-for fahr = 0, 20, ..., 300 */
+   for fahr = 0, 20, ..., 300 */
 
 main ()
 {
@@ -15,10 +15,10 @@ main ()
     step = 20;      /* step size */
 
     fahr = lower;
-    while (fahr <= upper) (
-            /* celsius = 5 * (fahr-32) / 9; --old */ 
-            celsius = (5.0/9.0) * (fahr-32.0);
-            printf("%3.0f %5.1f\n", fahr, celsius);
-            fahr = fahr + step;
-            )
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %5.1f\n", fahr, celsius);
+        fahr = fahr + step;    
+    }
 }
+
