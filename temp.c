d7049550a8a1626d8145e349dd2d4003811bea35
Tedley Meralus <tmeralus@gmail.com>
2024-04-02 21:29:37 -0400

    fixed syntax after local revert

--- temp.c
+++ temp.c
@@ -3,8 +3,7 @@
 /* print Fahrenheit-Celsius table 
 for fahr = 0, 20, ..., 300 */
 
-main ()
-{
+main (){
     /* using float instead of int for better accuracy.
      * using int doesnt give a decimal */
     float fahr, celsius;
@@ -15,10 +14,10 @@ main ()
     step = 20;      /* step size */
 
     fahr = lower;
-    while (fahr <= upper) (
+    while (fahr <= upper){
             /* celsius = 5 * (fahr-32) / 9; --old */ 
             celsius = (5.0/9.0) * (fahr-32.0);
             printf("%3.0f %5.1f\n", fahr, celsius);
             fahr = fahr + step;
-            )
+            }
 }
