54aba7b058d32cb411a409d5cfecf5f86d9a5b28
tedleyem <tmeralus@gmail.com>
2025-10-28 19:27:29 -0400

    updated download mirror and cleaned up script

--- get-kernel.sh
+++ get-kernel.sh
@@ -2,32 +2,25 @@
 set -e
 
 KERNEL_VERSION=6.12.30
-CURL="https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$KERNEL_VERSION.tar.xz"
+CURL="https://mirrors.edge.kernel.org/pub/linux/kernel/v6.x/linux-$KERNEL_VERSION.tar.xz"
 KFILE="linux-$KERNEL_VERSION.tar.xz"
 KDIR="linux-$KERNEL_VERSION"
 TARGETDIR="linux-kernel"
 
-echo "→ Checking for existing $KFILE ..."
-if [ -f "$KFILE" ]; then
-    echo "→ $KFILE already exists, skipping download."
-else
-    echo "→ Downloading Linux kernel $KERNEL_VERSION ..."
-    wget -c --show-progress "$CURL"
-fi
+echo "-- Preparing linux-kernel directory ..."
+mkdir -p "$KDIR"
 
-echo "→ Preparing linux-kernel directory ..."
-mkdir -p "$TARGETDIR"
+echo "-- Downloading Linux kernel $KERNEL_VERSION ..."
+wget -c --show-progress "$CURL"
 
-echo "→ Extracting $KFILE ..."
+echo "-- Extracting $KFILE"
 tar -xf "$KFILE"
 
-echo "→ Syncing files into $TARGETDIR ..."
+echo "-- Syncing files from $KDIR/ into $TARGETDIR/ "
 rsync -av --delete-after "$KDIR"/ "$TARGETDIR"/
 
-echo "→ Cleaning up ..."
-#echo "→ Removing $KFILE ..."
-#rm -f "$KFILE"
-echo "→ Removing $KDIR ..."
+echo "-- Cleanup $KFILE and $KDIR"
+rm -f "$KFILE"
 rm -rf "$KDIR"
 
 echo "Linux $KERNEL_VERSION synced into $TARGETDIR/"
