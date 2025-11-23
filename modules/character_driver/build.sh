ca3317dcfdefd39e8e2b748428a3fd43b0782c54
tedleyem <tmeralus@gmail.com>
2025-11-23 12:58:45 -0500

    updated build scripts

--- modules/character_driver/build.sh
+++ modules/character_driver/build.sh
@@ -1,10 +1,21 @@
-#!/bin/bash 
-#rm *.ko
-#rm *.o.cmd 
-#rm *.Modules*
-#rm *.module
-#rm *..module
-#rm *.mod
-#rm *.symvers
-#rm *.order
-sudo make -C /usr/src/linux-headers-6.12.30-amd64/ M=~/projects/c-programming/modules/character_driver/
+#!/bin/bash
+KERNEL_VERSION=$(uname -r)
+KERNEL_HEADERS="linux-headers-$KERNEL_VERSION"
+MODULE_DIR="~/projects/c-programming/modules/hellomodule/"
+
+# remove exisitng kernel file 
+rm -f *.ko *.o.cmd *.Modules* *.module *..module *.mod *.symvers *.order
+
+echo "$KERNEL_VERSION"
+# check and install linux-header files 
+if dpkg -l | grep -q "^ii.*${KERNEL_HEADERS}\s";then
+    echo "Linux Headers $KERNEL_HEADERS already installed, Skipping install"
+else
+    echo "Installing $KERNEL_HEADERS" && sleep 1
+    sudo apt install -y $KERNEL_HEADERS 
+fi
+
+# Build 
+echo "Kernel module compilation"
+make -C /usr/src/"$KERNEL_HEADERS"/ M="$MODULE_DIR"
+
